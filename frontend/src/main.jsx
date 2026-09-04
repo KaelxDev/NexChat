e32402192360f39783878aca554eb855b61d58f3
Kael <kaikeerrichelli@gmail.com>
2026-09-04 02:42:47 -0300

    feat: add visible DM notification center

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -27,11 +27,13 @@ import './PrivateDM.css'
 import './MobileShell.css'
 import './MobileDrawerFinal.css'
 import './DirectMessageNotification.css'
+import './DirectMessageNotificationCenter.css'
 import AutoMessageScroll from './AutoMessageScroll.jsx'
 import AppEdit from './AppEdit.jsx'
 import PrivateDMFeature from './components/PrivateDMFeature.jsx'
 import DirectMessageNotifier from './components/DirectMessageNotifier.jsx'
 import DirectMessageNotificationBridge from './components/DirectMessageNotificationBridge.jsx'
+import DirectMessageNotificationCenter from './components/DirectMessageNotificationCenter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -41,5 +43,6 @@ createRoot(document.getElementById('root')).render(
     <PrivateDMFeature />
     <DirectMessageNotifier />
     <DirectMessageNotificationBridge />
+    <DirectMessageNotificationCenter />
   </StrictMode>,
 )
